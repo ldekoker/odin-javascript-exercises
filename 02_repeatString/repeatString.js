0402bbb27c46f4a04edcc1eba1075eb60105f0cd
ldekoker <24773744+ldekoker@users.noreply.github.com>
2024-09-06 19:11:35 +1000

    Task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    if (times < 0) return "ERROR";
+    
+    let return_string = "";
+    for (let i = 0; i < times; i++) {
+        return_string = return_string.concat(string);
+    }
 
+    return return_string;
 };
 
 // Do not edit below this line
