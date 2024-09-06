f76d3493d83d26620d305c68be3c8eb136dd7172
ldekoker <24773744+ldekoker@users.noreply.github.com>
2024-09-06 19:46:07 +1000

    Task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 ) return "ERROR";
+    if (!(Number.isInteger(num1) && Number.isInteger(num2))) return "ERROR";
+
+    let a = (num1 >= num2)? num2 : num1;
+    let b = (num1 >= num2)? num1 : num2;
+    let sum = 0;
+
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
