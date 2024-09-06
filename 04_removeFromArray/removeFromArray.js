38f9d8430a1959954253046fe6af5c82ff0894d6
ldekoker <24773744+ldekoker@users.noreply.github.com>
2024-09-06 19:38:32 +1000

    Task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < array.length; i++){
+        for (const arg of args) {
+            if (array[i] === arg) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
