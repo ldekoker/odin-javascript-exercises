3ef3206f8e9515ef2f2316123bb424b1805d1ec6
ldekoker <24773744+ldekoker@users.noreply.github.com>
2024-09-06 19:17:17 +1000

    Task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse_array = []
 
+    for (let i = string.length - 1; i > -1; i--){
+        reverse_array.push(string.charAt(i));
+        }
+
+    return reverse_array.join('')
 };
 
 // Do not edit below this line
